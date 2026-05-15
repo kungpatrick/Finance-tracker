adf0cff2b2576375ff2a2b086fb064a0d17a46be
Patrickkung <kung.patrick@gmail.com>
2026-05-15 15:25:03 -0400

    Fix Custom Categories

--- backend/supabaseClient.ts
+++ backend/supabaseClient.ts
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '../types/supabase'; // Import the generated types
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
